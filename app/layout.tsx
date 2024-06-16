5557ca1841b6798b7226cf6ef1ad819898dcb098
Cyril-vtn <votion.cyril@gmail.com>
2024-06-16 17:12:14 +0200

    feat: Add Header component to layout.tsx
    The Header component is added to the layout.tsx file to display a header section on the page. This enhances the overall design and provides a consistent navigation experience for users.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from "./components/Header";
 
 
 export const metadata: Metadata = {
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="scrollbar-hide">
-      <body className="scrollbar-hide">{children}</body>
+      <body className="scrollbar-hide">
+      <Header />
+
+        {children}
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,12 @@
 
 import "./globals.css";
 
-import { Header } from "./components/Header";
 import { ScrollSection } from "./components/ScrollSection";
 
 export default function Home() {
-
-
   return (
     <main className="overflow-hidden">
       <div className="bg-brandBlue bg-noise-bg fixed top-0 left-0 w-full h-full"></div>
-      <Header />
       <ScrollSection />
     </main>
   );
