ff5546208808f1657b0120f169924848c3f1b122
Cyril-vtn <votion.cyril@gmail.com>
2024-06-16 16:44:54 +0200

    remove old commented code

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,6 @@ export default function Home() {
       <div className="bg-brandBlue bg-noise-bg fixed top-0 left-0 w-full h-full"></div>
       <Header />
       <ScrollSection />
-      {/* <div className="h-full flex justify-center items-center" style={{ minHeight: "calc(100vh - 192px)" }}>
-        <h1 style={{ fontSize: "clamp(2rem, 11vw, 20rem)" }} className="font-marlin-bold text-brandOffwhite text-center">
-          Cyril Votion
-        </h1>
-      </div> */}
     </main>
   );
 }
