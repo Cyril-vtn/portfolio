ed67988a2da93b6fa10cec24ad1b7300dd8de991
Cyril-vtn <votion.cyril@gmail.com>
2024-06-16 17:13:06 +0200

    add href as props and check if link is active using next feature

--- app/components/NavigationButton.tsx
+++ app/components/NavigationButton.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export const NavigationButton = ({ text }: { text: string }) => {
+export const NavigationButton = ({
+  text,
+  href,
+}: {
+  text: string;
+  href: string;
+}) => {
+  const pathname = usePathname();
   return (
-    <li>
-      <div className=" py-2 px-[10px] hover:bg-brandDeepBlue rounded-lg transition duration-300 ease-in-out hover:shadow-soft hover:scale-105 cursor-pointer hover:bg-opacity-50">
-        <span>{text}</span>
-      </div>
-    </li>
+    <Link
+      href={href}
+      className={`py-2 px-[10px] rounded-lg transition duration-300 ease-in-out cursor-pointer ${
+        pathname === href
+          ? "bg-brandOffwhite text-brandBlue"
+          : "hover:text-brandOffwhite hover:shadow-soft hover:scale-105 hover:bg-opacity-50 hover:bg-brandDeepBlue "
+      }`}
+    >
+      <span>{text}</span>
+    </Link>
   );
 };
