ee1a9b8e53d5b510ed288e2d9b3d0af0fecf90d9
Cyril-vtn <votion.cyril@gmail.com>
2024-06-17 23:57:20 +0200

    remove unseless div container aroudn sidebar to fix animation behavior

--- app/components/ScrollSection.tsx
+++ app/components/ScrollSection.tsx
@@ -75,7 +75,7 @@ export const ScrollSection = () => {
       };
 
       document.addEventListener("click", handleClickOutside);
-
+      console.log("sidebarOpen", sidebarOpen);
       return () => {
         document.removeEventListener("click", handleClickOutside);
       };
@@ -127,64 +127,60 @@ export const ScrollSection = () => {
               >
                 <BsGithub size={18} />
               </button>
+              {/* SIDEBAR */}
               <div
-                className={`absolute right-0 w-1/4 h-full rounded-r-xl z-50 p-4 transition-all duration-300 ease-in-out ${
-                  sidebarOpen ? "right-0" : "-right-1/4"
+                id="sidebar"
+                ref={sidebarRef}
+                className={`absolute z-50 m-4 top-0 flex flex-col h-auto w-1/4 gap-4 bg-brandOffwhite bg-noise-bg rounded-lg shadow-hard py-3 px-5 overflow-auto scrollbar-default transition-all duration-300 ease-in-out ${
+                  sidebarOpen ? "right-0" : "-right-96"
                 }`}
               >
-                {/* SIDEBAR */}
-                <div
-                  ref={sidebarRef}
-                  className="flex flex-col h-auto w-full gap-4 bg-brandOffwhite bg-noise-bg rounded-lg shadow-hard py-3 px-5 overflow-auto scrollbar-default "
-                >
-                  <div className="flex flex-col rounded-t-lg w-full h-full gap-4">
-                    <div className="flex flex-col  items-center gap-2">
-                      <div className="flex gap-2 justify-between w-full">
-                        <h3 className="font-marlin-medium text-brandBlue text-md tracking-normal">
-                          Fonctionnalités
-                        </h3>
-                        <FaArrowRight
-                          size={25}
-                          className="cursor-pointer transition-all duration-200 ease-in-out p-1 hover:bg-brandBlue text-brandBlue hover:text-brandOffwhite rounded-full"
-                          onClick={() => setSidebarOpen(false)}
-                        />
-                      </div>
-                      <ul className="text-sm space-y-1 text-lightGray">
-                        <li>- Création d’utilisateur</li>
-                        <li>
-                          - Changement de Photo de profil / nom / mot de passe
-                        </li>
-                        <li>
-                          - Progressions de l’utilisateur via des succès ainsi
-                          qu’une barre d’xp
-                        </li>
-                        <li>
-                          - Sauvegarde des données via Firebase (Firestore,
-                          Authentication, Storage)
-                        </li>
-                        <li>
-                          - Partage de profil via React Router et ses routes
-                          dynamiques
-                        </li>
-                        <li>- Responsive design (Desktop First & Tablette)</li>
-                      </ul>
-                    </div>
-                    {/* liste des fonctionnalité */}
-
-                    <div className="flex flex-col gap-2">
+                <div className="flex flex-col rounded-t-lg w-full h-full gap-4">
+                  <div className="flex flex-col  items-center gap-2">
+                    <div className="flex gap-2 justify-between w-full">
                       <h3 className="font-marlin-medium text-brandBlue text-md tracking-normal">
-                        Languages utilisés
+                        Fonctionnalités
                       </h3>
-                      <ul className="text-sm space-y-1 text-lightGray">
-                        <li>- React</li>
-                        <li>- React Router</li>
-                        <li>- Firebase</li>
-                        <li>- MUI</li>
-                      </ul>
+                      <FaArrowRight
+                        size={25}
+                        className="cursor-pointer transition-all duration-200 ease-in-out p-1 hover:bg-brandBlue text-brandBlue hover:text-brandOffwhite rounded-full"
+                        onClick={() => setSidebarOpen(false)}
+                      />
                     </div>
+                    <ul className="text-sm space-y-1 text-lightGray">
+                      <li>- Création d’utilisateur</li>
+                      <li>
+                        - Changement de Photo de profil / nom / mot de passe
+                      </li>
+                      <li>
+                        - Progressions de l’utilisateur via des succès ainsi
+                        qu’une barre d’xp
+                      </li>
+                      <li>
+                        - Sauvegarde des données via Firebase (Firestore,
+                        Authentication, Storage)
+                      </li>
+                      <li>
+                        - Partage de profil via React Router et ses routes
+                        dynamiques
+                      </li>
+                      <li>- Responsive design (Desktop First & Tablette)</li>
+                    </ul>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h3 className="font-marlin-medium text-brandBlue text-md tracking-normal">
+                      Languages utilisés
+                    </h3>
+                    <ul className="text-sm space-y-1 text-lightGray">
+                      <li>- React</li>
+                      <li>- React Router</li>
+                      <li>- Firebase</li>
+                      <li>- MUI</li>
+                    </ul>
                   </div>
                 </div>
               </div>
+
               <div
                 id="overlay"
                 className={`absolute w-full h-full z-40 p-2 transition-all duration-300 ease-in-out ${
