3c8d4e6034fee6b1cd11cb687e4b7731f1eb956f
Cyril-vtn <votion.cyril@gmail.com>
2024-06-17 23:37:13 +0200

    adjust border radius and set text props to optional

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,10 +4,10 @@ export const Button = ({
   text,
   icon,
 }: {
-  text: string;
+  text?: string;
   icon?: React.ReactNode;
 }) => {
-  return <button className="py-1 px-5 bg-brandOffwhite text-brandBlue rounded-lg flex items-center gap-2 transition duration-300 ease-in-out hover:shadow-soft hover:scale-105 text-xs font-marlin-medium leading-7 tracking-normal">
+  return <button className="py-1 px-5 bg-brandOffwhite text-brandBlue rounded-[10px] flex items-center gap-2 transition duration-300 ease-in-out hover:shadow-soft hover:scale-105 text-xs font-marlin-medium leading-7 tracking-normal">
     {icon}
     <span>{text}</span>
   </button>;
