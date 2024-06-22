8009a5d622d72423bbc9e126e79ed636e29d12dc
Cyril-vtn <votion.cyril@gmail.com>
2024-06-22 18:06:11 +0200

    use Lenis to add smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 
 import "./globals.css";
-
 import { ScrollSection } from "./components/ScrollSection";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    const raf = (time) => {
+      lenis.raf(time);
+
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className="overflow-hidden">
       <ScrollSection />
