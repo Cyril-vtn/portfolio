246a2dfa934cf7cf0d1d575691da76a579227fa7
Cyril-vtn <votion.cyril@gmail.com>
2024-06-26 23:15:13 +0200

    setup about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>page</div>
-  )
+    <main className="overflow-hidden w-full h-full">
+      <section className="h-full w-full pt-[100px] relative"></section>
+    </main>
+  );
 }
