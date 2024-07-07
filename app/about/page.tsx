b5a2d1c2c2d99825bc5d74233d11ba86926bc090
Cyril-vtn <votion.cyril@gmail.com>
2024-07-07 18:43:36 +0200

    refactor: Update About page to include infinite moving icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import { FaCss3Alt, FaReact } from "react-icons/fa";
+import { RiNextjsFill, RiTailwindCssLine } from "react-icons/ri";
+import { SiNestjs, SiTypescript } from "react-icons/si";
+import { InfiniteMovingIcons } from "../components/infinite-moving-icons";
+import { DiJavascript } from "react-icons/di";
+import { FiFigma } from "react-icons/fi";
+import { PiFramerLogo } from "react-icons/pi";
 
 export default function About() {
   return (
     <main className="overflow-hidden w-full h-full">
-      <section className="h-full w-full pt-[100px] relative"></section>
+          <InfiniteMovingIcons
+            speed="fast"
+            icons={[
+              <FaReact key="react" />,
+              <FaCss3Alt key="css" />,
+              <RiTailwindCssLine key="tailwind" />,
+              <SiTypescript key="typescript" />,
+              <SiNestjs key="nestjs" />,
+              <RiNextjsFill key="nextjs" />,
+              <DiJavascript key="javascript" />,
+              <FiFigma key="figma" />,
+              <PiFramerLogo key="framer" />,
+            ]}
+          />
     </main>
   );
 }
---
+++ app/components/infinite-moving-icons.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { cn } from "../utils/cn";
+import React, { useEffect, useState } from "react";
+import { FollowerPointerCard } from "./following-pointer";
+
+export const InfiniteMovingIcons = ({
+  icons,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: {
+  icons: React.ReactNode[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const addAnimation = () => {
+      if (containerRef.current && scrollerRef.current) {
+        const scrollerContent = Array.from(scrollerRef.current.children);
+
+        scrollerContent.forEach((item) => {
+          const duplicatedItem = item.cloneNode(true);
+          if (scrollerRef.current) {
+            scrollerRef.current.appendChild(duplicatedItem);
+          }
+        });
+
+        getDirection();
+        getSpeed();
+        setStart(true);
+      }
+    };
+
+    addAnimation();
+  }, []);
+
+  const [start, setStart] = useState(false);
+
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn("scroller relative z-20  bg-brandOffwhite", className)}
+    >
+      <div
+        className="absolute -top-[60px] z-50 left-0 w-full h-full bg-noise-bg rotate-180"
+        style={{
+          backgroundImage: `url('/header-pattern.svg')`,
+          backgroundSize: "contain",
+          backgroundRepeat: "repeat-x",
+          content: '""',
+        }}
+      ></div>
+
+      <div
+        className="absolute -bottom-[60px] z-40 left-0 w-full h-full bg-noise-bg"
+        style={{
+          backgroundImage: `url('/header-pattern.svg')`,
+          backgroundSize: "contain",
+          backgroundRepeat: "repeat-x",
+          content: '""',
+        }}
+      ></div>
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          " flex min-w-full shrink-0 gap-4 py-1 md:py-2 w-max flex-nowrap",
+          start && "animate-scroll ",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {icons.map((item, idx) => (
+          <div key={idx} className="flex items-center gap-2 z-50">
+            <li
+              className="relative flex-shrink-0 px-8 py-1 text-brandBlue text-2xl md:text-4xl font-marlin-bold"
+              key={idx}
+            >
+              {item}
+            </li>
+            <li
+              className="
+              text-brandBlue text-1xl md:text-2xl font-marlin-bold"
+            >
+              {" "}
+              -{" "}
+            </li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
