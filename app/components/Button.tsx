895426ab4d73abe5e872646441e1415003b38f42
Cyril-vtn <votion.cyril@gmail.com>
2024-07-08 18:52:33 +0200

    refactor: Improve Button component with cursor prop and dynamic styling

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,12 +3,19 @@ import React from "react";
 export const Button = ({
   text,
   icon,
+  cursor = "pointer",
 }: {
   text?: string;
   icon?: React.ReactNode;
+  cursor?: "pointer" | "none";
 }) => {
-  return <button className="py-1 px-5 bg-brandOffwhite text-brandBlue rounded-[10px] flex items-center gap-2 transition duration-300 ease-in-out hover:shadow-soft hover:scale-105 text-xs font-marlin-medium leading-7 tracking-normal">
-    {icon}
-    <span>{text}</span>
-  </button>;
+  return (
+    <button
+      className={`cursor-none py-1 px-5 bg-brandOffwhite text-brandBlue rounded-[10px] flex items-center gap-2 transition duration-300 ease-in-out hover:shadow-soft hover:scale-105 text-xs font-marlin-medium leading-7 tracking-normal`}
+      style={{ cursor }}
+    >
+      {icon}
+      <span>{text}</span>
+    </button>
+  );
 };
