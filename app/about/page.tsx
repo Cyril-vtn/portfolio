e948ae20cfa7fccca4d465d7da787f0e6cdcc392
Cyril-vtn <votion.cyril@gmail.com>
2024-07-08 18:05:02 +0200

    refactor: Add MongoDB and PostgreSQL icons to About page and add description

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ import { FaCss3Alt, FaReact } from "react-icons/fa";
 import { RiNextjsFill, RiTailwindCssLine } from "react-icons/ri";
 import { SiNestjs, SiTypescript } from "react-icons/si";
 import { InfiniteMovingIcons } from "../components/infinite-moving-icons";
-import { DiJavascript } from "react-icons/di";
+import { DiJavascript, DiMongodb, DiPostgresql } from "react-icons/di";
 import { FiFigma } from "react-icons/fi";
 import { PiFramerLogo } from "react-icons/pi";
 import {
@@ -50,6 +50,8 @@ export default function About() {
               <DiJavascript key="javascript" />,
               <FiFigma key="figma" />,
               <PiFramerLogo key="framer" />,
+              <DiMongodb key="mongodb" />,
+              <DiPostgresql key="postgresql" />,
             ]}
           />
           <div className="absolute z-20 w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[300px]">
@@ -64,16 +66,15 @@ export default function About() {
               />
             </FollowerPointerCard>
           </div>
-        </div>{" "}
-        <p className="text-center text-xl text-brandOffwhite xl:text-3xl z-10 max-w-7xl px-9 ">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          vehicula, nulla nec vehicula ultricies, urna ex dictum lacus, in
-          vestibulum felis nunc et justo. Nam sed tincidunt neque. Integer vitae
-          justo nec sapien vehicula dictum. Nullam nec odio et nisl sollicitudin
-          ultricies. Nulla facilisi. Suspendisse potenti. Donec et nunc nec nunc
-          tincidunt fermentum. Nullam nec odio et nisl sollicitudin ultricies.
-          Nulla facilisi. Suspendisse potenti. Donec et nunc nec nunc tincidunt
-          fermentum.
+        </div>
+        <p className="text-center text-xl text-brandOffwhite xl:text-3xl z-10 max-w-7xl px-9 tracking-wider">
+          Développeur Full Stack (orienté Front-End) passionné par les nouveaux
+          techniques et les défis techniques. J&apos;aime apprendre et partager
+          mes connaissances. Toujours des idées plein la tête, je suis un
+          créatif curieux et enthousiaste. Toujours à la recherche de nouveaux
+          projets intéressants et de nouvelles opportunités. N&apos;hésitez pas
+          à me contacter si vous avez un projet ou une idée à partager. Je
+          serais ravi de vous aider à les concrétiser !
         </p>
       </section>
     </main>
