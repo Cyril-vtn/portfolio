34ce40586907fff9c8540bed56da402d4b32f85f
Cyril-vtn <votion.cyril@gmail.com>
2024-07-13 20:47:19 +0200

    feat: Add Resume page with download button and resume image

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,15 +4,18 @@ export const Button = ({
   text,
   icon,
   cursor = "pointer",
+  onClick,
 }: {
   text?: string;
   icon?: React.ReactNode;
   cursor?: "pointer" | "none";
+  onClick?: () => void;
 }) => {
   return (
     <button
       className={`cursor-none py-1 px-5 bg-brandOffwhite text-brandBlue rounded-[10px] flex items-center gap-2 transition duration-300 ease-in-out hover:shadow-soft hover:scale-105 text-xs font-marlin-medium leading-7 tracking-normal`}
       style={{ cursor }}
+      onClick={onClick}
     >
       {icon}
       <span>{text}</span>
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,13 +4,24 @@ import React, { useEffect, useState } from "react";
 import { Button } from "../components/Button";
 import { BsLinkedin } from "react-icons/bs";
 import { FiMail } from "react-icons/fi";
+import Image from "next/image";
 
 export default function Resume() {
   return (
     <main className="overflow-hidden w-full h-full relative">
-      <section className="h-screen w-full flex flex-col gap-8 py-12 justify-center items-center">
-        {/* <iframe src="/resume.pdf" className="w-full h-full" /> */}
-        <Button text="Télécharger le CV" icon={<FiMail size={18} />} />
+      <section className="h-full w-full flex flex-col gap-8 pt-28 justify-center items-center px-11">
+        <Image
+          src="/CYRIL_VOTION_RESUME_FR.png"
+          className="rounded-lg"
+          alt="Cyril Votion resume"
+          width={595}
+          height={842}
+        />
+        <Button
+          text="Télécharger le CV"
+          icon={<FiMail size={18} />}
+          onClick={() => window.open("/CYRIL_VOTION_RESUME_FR.pdf")}
+        />
       </section>
     </main>
   );
