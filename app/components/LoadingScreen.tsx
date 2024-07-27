7b2000aec951fa7d35506493d2fe0c8a3caac98a
Cyril-vtn <votion.cyril@gmail.com>
2024-07-27 16:21:30 +0200

    refactor: Update LoadingScreen component to fix typo in className and add h-dvh class for height

--- app/components/LoadingScreen.tsx
+++ app/components/LoadingScreen.tsx
@@ -24,7 +24,7 @@ export const LoadingScreen = ({ children }) => {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 w-screen max-h-screen h-screen flex justify-center items-center ${
+        className={`fixed top-0 left-0 w-screen h-dvh max-h-screen flex justify-center items-center ${
           loading ? "opacity-100" : "opacity-0"
         } 
       } transition-opacity duration-500 ease-in-out`}
